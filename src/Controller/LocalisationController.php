ae27cef475005a2141bb6d38cd3e315fbf42c7b9
hocineidir <hocine.idir@telecom-supdaris.eu>
2019-03-12 12:33:31 +0100

    use statement in LocalisationController.php

--- src/Controller/LocalisationController.php
+++ src/Controller/LocalisationController.php
@@ -7,6 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\LocalisationRepository;
+use App\Entity\Localisation;
 
 /**
  * @Route("/localisation")
